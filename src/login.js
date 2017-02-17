6836c1b20870e278facf07c5a18b76405d49af64
jaredmey <meyer.jared@uwlax.edu>
2017-02-17 12:05:09 -0600

    Created new Register tab

---
+++ src/login.js
@@ -0,0 +1,9 @@
+function openRegisterTab(event) {
+    document.querySelector('.account-wall-login').style.display = "none";
+    document.querySelector('.account-wall-register').style.display = "inline";
+}
+
+function openLoginTab(event) {
+    document.querySelector('.account-wall-register').style.display = "none";
+    document.querySelector('.account-wall-login').style.display = "inline";
+}
