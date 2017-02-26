d40ee67663dace7a571726446fbd1c972f4f328e
mattbehrens05 <matthewbehrens@Matthews-MacBook-Pro-3.local>
2017-02-25 19:50:39 -0600

    added php file

---
+++ src/loginUsers.php
@@ -0,0 +1,21 @@
+<?php
+if (isset($_POST['submit'])) {
+    
+    $mysqli = mysqli_connect("localhost", "user", "password", "JEMDental");
+    
+    if ($mysqli->connect_errno) {
+        echo 'Cannot connect to server';
+    }
+    
+    $Email = $_POST['email'];
+    $Name = $_POST['name'];
+    $Username = $_POST['username'];
+    $Password = $_POST['password'];
+    
+    $sql = "INSERT INTO `Users` (`Name`, `Email`, `Username`, `Password`) VALUES ('$Name', '$Email', '$Username', '$Password')";
+    
+    mysqli_query($mysqli, $sql);
+    
+    mysqli_close($mysqli);
+}
+?>
