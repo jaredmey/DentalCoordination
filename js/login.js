14c5ba7331a44f20f3351566b55164a785afe2b1
mattbehrens05 <matthewbehrens@Matthews-MacBook-Pro-3.local>
2017-02-26 19:23:26 -0600

    I can't get it to work

--- js/login.js
+++ js/login.js
@@ -7,3 +7,29 @@ function openLoginTab(event) {
     document.querySelector('.account-wall-register').style.display = "none";
     document.querySelector('.account-wall-login').style.display = "inline";
 }
+//changes what happens on the submit
+
+
+$('#logInForm').submit(function(){
+    logIn();
+    return false;
+});
+function logIn(){    
+    var username=$("#username").val();
+    console.log(username);
+    var password=$("#password").val();
+    //admin account
+    if((username === "Admin") && (password === "Admin")){
+        
+    }
+    //JaneDoe account
+    if((username === "JaneDoe") && (password === "JaneDoe")){
+        window.location.href = "DentalCoordination/appointment_form.html";
+        console.log("In JD account");
+    }
+    //Doctor Dan
+    if((username === "DoctorDan") && (password === "DoctorDan")){
+        window.location.href = "DentalCoordination/employee_schedule.html";
+    }
+    
+}
