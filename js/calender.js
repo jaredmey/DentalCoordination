cf8a96267466e9cc52fa8c466608758ebcbe7222
mattbehrens05 <matthewbehrens@Matthews-MacBook-Pro-3.local>
2017-03-25 15:27:52 -0500

    got rid of bs calendar code

--- js/calender.js
+++ js/calender.js
@@ -1,20 +1,28 @@
 //use this funciton after button press to populate table based on emplyee / day / time constrints
 function populateCalendar(){
     var list=$("table#calendarTable");
-    
-    
+    //table header
+    list.append(
+        '<tr>' +
+        //add data in td's
+            '<th>' +"Time"+ '</th>' +
+            '<th>' +"Employee"+ '</th>' +
+            '<th>' +"Customer"+ '</th>' +
+            '<th>' + '</th>' +
+        '</tr>'
+    );
     //use for loop to append all times in day
     //grab from server
-    //add table header before loop with same format
-    list.append(
-    '<tr>' +
-        //add data in td's
-        '<td>' + '</td>' +
-        '<td>' + '</td>' +
-        '<td>' + '</td>' +
-        '<td>' + '</td>' +
-    '</tr>'
-    );
+    
+        list.append(
+         '<tr>' +
+            //add data in td's
+            '<td>' + '</td>' +
+            '<td>' + '</td>' +
+            '<td>' + '</td>' +
+            '<td>' + '</td>' +
+        '</tr>'
+        );
 }
 //use this method to delete the displayed calender before populating with populateCalendar
 function removeCalendarRows(){
@@ -22,4 +30,11 @@ function removeCalendarRows(){
     while(list.hasChildNodes()){
         list.removeChild(list.lastChild);
     }
+}
+
+function findAppointments(){
+    removeCalendarRows();
+    //grab the selectors data
+    //if none selected don't call populateCalendar
+    populateCalendar();
 }
