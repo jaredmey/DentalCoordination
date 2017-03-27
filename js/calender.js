a869827e65bcc593d0be7d22437c241b2c1b756a
mattbehrens05 <matthewbehrens@Matthews-MacBook-Pro-3.local>
2017-03-26 23:51:25 -0500

    added example appointment and button

--- js/calender.js
+++ js/calender.js
@@ -1,7 +1,7 @@
-//use this function after button press to populate table based on employee / day / time constrints
-//this functions takes a parameter of the time of the added appointment
-
 var timeslots = [8,9,10,11,12,1,2,3,4]
+//example appointments struture
+var appointment1 = {day:" ",time:9,empl:"Dr. Behrens",customer:"Nark"};
+var appts = [appointment1];
 function populateCalendar(dateString,Employee){
     
     var list=$("table#calendarTable");
@@ -31,7 +31,28 @@ function populateCalendar(dateString,Employee){
         
             //must check array form database to see if the timeslot is taken
             //if(timeslots[i] is in the array) --- fill the slot with the info
-            //else
+        if((timeslots[i]===appts[0].time)){
+            var time;
+            //Show am or pm
+            if (timeslots[i]>4){
+                time = timeslots[i]+":00 AM"
+            }
+            else{
+                time = timeslots[i]+":00 PM"
+            }
+
+            list.append(
+             '<tr id="'+timeslots[i]+'">' +
+                //add data in td's
+                '<td>' +time+ '</td>' +
+                '<td>' +appts[0].empl+ '</td>' +
+                '<td>' +appts[0].customer+ '</td>' +
+                '<td>' +'<span id="editAppointment'+timeslots[i]+'" class="btn btn-primary" onclick="editAppointment('+timeslots[i]+')">Edit appointment</span>'+ '</td>' +
+            '</tr>'
+            );
+        }
+            //else do below
+        else{
             var time;
             //Show am or pm
             if (timeslots[i]>4){
@@ -50,6 +71,7 @@ function populateCalendar(dateString,Employee){
                 '<td>' +'<span id="addAppointment'+timeslots[i]+'" class="btn btn-success" onclick="addAppointment('+timeslots[i]+')">Add appointment</span>'+ '</td>' +
             '</tr>'
             );
+        }
     }
     
 }
