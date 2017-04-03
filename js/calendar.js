1ae3b6ca434d81426011f32db4e4187c91033c0a
mattbehrens05 <matthewbehrens@Matthews-MacBook-Pro-3.local>
2017-04-03 15:34:36 -0500

    frick

--- js/calendar.js
+++ js/calendar.js
@@ -1,34 +1,41 @@
 //must set this after successful login. Either a string or number
-alert(userType);
-alert(username);
+//alert(userType);
+//alert(username);
 
 //the user that is logged in. For appointment puposes
 var User;
 
 //must make methods to show/hide certian things/buttons based on the user type
-var timeslots = [8,9,10,11,12,1,2,3,4];
+var timeslots = [8, 9, 10, 11, 12, 1, 2, 3, 4];
 
 //example appointments struture
-var appointment1 = {day:" ",time:9,empl:"Dr. Behrens",customer:"Nark"};
-var appts = [appointment1];
-function populateCalendar(dateString,Employee){
+var appointmentBlank = {day: "x", time: 0, empl: "x", customer: "x"};
+var appts = [];
+function populateCalendar(dateString, Employee) {
+    //clear appts
+    var i, j, time, list = $("table#calendarTable");
+    for (j = 0; j < timeslots.length; j = j + 1) {
+        appts.push(appointmentBlank);
+        appts[j].time = Number(timeslots[j]);
+        console.log(appts[j].time);
+    }
     
-    var list=$("table#calendarTable");
     //table header
     list.append(
         '<tr>' +
         //add data in td's
-            '<th>' +"Time"+ '</th>' +
-            '<th>' +"Employee"+ '</th>' +
-            '<th>' +"Customer"+ '</th>' +
+            '<th>' + "Time" + '</th>' +
+            '<th>' + "Employee" + '</th>' +
+            '<th>' + "Customer" + '</th>' +
             '<th>' + '</th>' +
-        '</tr>'
+            '</tr>'
     );
     //
     //
     //
     //
     //This is where you take the two parameters (dateString,Employee) to load all of the corresponding appointments into an array or object array.
+    //sql call to grab appointments 'where employee = Employee and date = dateString' 
     //
     //
     //
@@ -36,77 +43,76 @@ function populateCalendar(dateString,Employee){
     //
     //
     //use for loop to append all times in day
-    for( var i=0;i<timeslots.length;i++){
-        
-            //must check array form database to see if the timeslot is taken
-            //if(timeslots[i] is in the array) --- fill the slot with the info
-        if((timeslots[i]===appts[0].time)){
-            var time;
+    
+    for (i = 0; i < timeslots.length; i  = i + 1) {
+        //must check array form database to see if the timeslot is taken
+        //if(timeslots[i] is in the array) --- fill the slot with the info
+        //check all appts in array not just 0
+        if (timeslots[i] === appts[i].time) {
             //Show am or pm
-            if (timeslots[i]>4){
-                time = timeslots[i]+":00 AM"
-            }
-            else{
-                time = timeslots[i]+":00 PM"
+            if (timeslots[i] > 4) {
+                time = timeslots[i] + ":00 AM";
+            } else {
+                time = timeslots[i] + ":00 PM";
             }
 
             list.append(
-             '<tr id="'+timeslots[i]+'">' +
+                '<tr id="' + timeslots[i] + '">' +
                 //add data in td's
-                '<td>' +time+ '</td>' +
-                '<td>' +appts[0].empl+ '</td>' +
-                '<td>' +appts[0].customer+ '</td>' +
-                '<td>' +'<span id="deleteAppointment'+timeslots[i]+'" class="btn btn-danger" onclick="deleteAppointment('+timeslots[i]+')">Delete appointment</span>'+ '</td>' +
-            '</tr>'
+                    '<td>' + time + '</td>' +
+                    '<td>' + appts[i].empl + '</td>' +
+                    '<td>' + appts[i].customer + '</td>' +
+                    '<td>' + '<span id="deleteAppointment' + timeslots[i] + '" class="btn btn-danger" onclick="deleteAppointment(' + timeslots[i] + ')">Delete appointment</span>' + '</td>' +
+                    '</tr>'
             );
         }
             //else do below
-        else{
-            var time;
+            else {
+            //console.log("TS= "+timeslots[i]+" appts = "+appts[i].time);
+            
             //Show am or pm
-            if (timeslots[i]>4){
-                time = timeslots[i]+":00 AM"
-            }
-            else{
-                time = timeslots[i]+":00 PM"
-            }
+                if (timeslots[i] > 4) {
+                    time = timeslots[i] + ":00 AM";
+                } else {
+                    time = timeslots[i] + ":00 PM";
+                }
 
-            list.append(
-             '<tr id="'+timeslots[i]+'">' +
+                list.append(
+                    '<tr id="' + timeslots[i] + '">' +
                 //add data in td's
-                '<td>' +time+ '</td>' +
-                '<td>' +"Available"+ '</td>' +
-                '<td>' +"Available"+ '</td>' +
-                '<td>' +'<span id="addAppointment'+timeslots[i]+'" class="btn btn-success" onclick="addAppointment('+timeslots[i]+')">Add appointment</span>'+ '</td>' +
-            '</tr>'
-            );
-        }
+                        '<td>' + time + '</td>' +
+                        '<td>' + "Available" + '</td>' +
+                        '<td>' + "Available" + '</td>' +
+                        '<td>' + '<span id="addAppointment' + timeslots[i] + '" class="btn btn-success" onclick="addAppointment(' + timeslots[i] + ')">Add appointment</span>'+ '</td>' +
+                        '</tr>'
+                );
+            }
     }
     
 }
 //use this method to delete the displayed calender before populating with populateCalendar
-function removeCalendarRows(){
-    var list =document.getElementById("calendarTable");
-    while(list.hasChildNodes()){
+function removeCalendarRows() {
+    var list = document.getElementById("calendarTable");
+    while (list.hasChildNodes()) {
         list.removeChild(list.lastChild);
     }
 }
 
-function findAppointments(){
+function findAppointments() {
     removeCalendarRows();
-    var h2=document.getElementById("displayInfo"); //grab the selectors data
+    var h2 = document.getElementById("displayInfo"); //grab the selectors data
     var e = document.getElementById("employeePicker");
-    var strUser = e.options[e.selectedIndex].text;
-    console.log(strUser);
-    var date =document.getElementById("date");
+    var employee = e.options[e.selectedIndex].text;
+    console.log(employee);
+    var date = document.getElementById("date");
     console.log(date.value);
     //if none selected don't call populateCalendar
-    if((date.value ==="")||(strUser==="")){
+    if( (date.value  === "") || (employee === "") ) {
         h2.textContent="Please Select Employee and date";
     }
     else{
-        populateCalendar(date.value,strUser);
-        h2.textContent="Appointments on "+date.value+" with "+strUser
+        populateCalendar(date.value,employee);
+        h2.textContent="Appointments on "+date.value+" with "+employee;
     }
     
 }
@@ -114,9 +120,9 @@ function addAppointment(time){
     //this grabs the row of the button pressed
     var row=document.getElementById(""+time)
     //example on how to change data
-    row.firstChild.nextSibling.nextSibling.nextSibling.textContent="Not Available"
+    row.firstChild.nextSibling.nextSibling.nextSibling.textContent="Not Available";
     //removing add button after press
-    $("#addAppointment"+time).remove();
+    $("#addAppointment" + time).remove();
     //
     //
     //
@@ -124,7 +130,9 @@ function addAppointment(time){
     //
     //
     //
+    //re populate the page
 }
-function deleteAppointment(time){
-    
+function deleteAppointment(time) {
+    //delete the appointment
+    //re populate the page - may need little wait
 }
--- js/login.js
+++ js/login.js
@@ -10,25 +10,4 @@ function openLoginTab(event) {
 
 
 
-//this fuction is in place of the submit button. Change button to type submit after
-function logIn(){    
-    var username=$("#username").val();
-    console.log(username);
-    var password=$("#password").val();
-    //admin account
-    if((username === "Admin") && (password === "Admin")){
-        //I spelled calendar wrong lol
-        window.location = "calender.html";
-        
-    }
-    //JaneDoe account
-    if((username === "JaneDoe") && (password === "JaneDoe")){
-        window.location = "appointment_form.html";
-        
-    }
-    //Doctor Dan
-    if((username === "DoctorDan") && (password === "DoctorDan")){
-        window.location = "employee_schedule.html";
-    }
-    
-}
+
