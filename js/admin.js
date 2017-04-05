ebc86b3b94c4c8df39c314bbbc3ae2b0bc843285
mattbehrens05 <matthewbehrens@Matthews-MacBook-Pro-3.local>
2017-04-05 15:19:22 -0500

    admin page
    Added helper methods for adding and deleting.

--- js/admin.js
+++ js/admin.js
@@ -1,26 +1,46 @@
-function findEmployees(){
-    var e = document.getElementById("employeePicker");
-    var employee = e.options[e.selectedIndex].text;
-}
 var Employees;
 function populateEmployees(){
+    
+    //removeEmployeeRows();
     //Make db call retrieve the employees type=? only. Grab the names only.
-    var i;
+    var i,list = $("#employeeTable");
+    removeEmployeeRows();
     //replace this later
-    var data=["Dr Behrens","Dr Meyer", "Dr Porzio"];
+    Employees=["Dr Behrens","Dr Meyer", "Dr Porzio"];
     //put this in the success finction
-    for (i=0;i<data.length;i++){
+    list.append(
+        '<tr>'+
+        '<th>' + "Employee Name" + '</th>' +
+        '<th>' + "" + '</th>' +
+        '</tr>'
+        );
+    for (i=0;i<Employees.length;i++){
         list.append(    
-        '<tr id="' + "employee"+i+ + '">' +
-        '<td>' + data[i] + '</td>' +
-        '<td>' + '<span id="deleteEmployee' + i + '" class="btn btn-danger" onclick="deleteEmployee(' + i + ')">Delete Employee</span>' + '</td>' +
+        '<tr>' +
+        '<td id="' + "employee"+i + '">' + Employees[i] + '</td>' +
+        '<td>' + '<span class="btn btn-lg btn-danger" onclick="deleteEmployee('+i+')">Delete Employee</span>' + '</td>' +
         '</tr>'
         );
     }
 }
 function deleteEmployee(position){
+    EmployeeName = $("#employee"+position).text();
+    console.log(EmployeeName);
+    //delete employee where Name=EmployeeName
+    //DB Call
     
+    populateEmployees();
 }
 function addEmployee(){
-    
+    var name=$("name"), username=$("username"), password=$("password"), userType=$("UserType");
+    //figure out how to get all the values from the above
+    //add to user DB table
+    //call last
+    populateEmployees();
+}
+function removeEmployeeRows() {
+    var list = document.getElementById("employeeTable");
+    while (list.hasChildNodes()) {
+        list.removeChild(list.lastChild);
+    }
 }
