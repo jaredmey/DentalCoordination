f6c626ad62f81636f47b42cc630093b90faec347
jaredmey <meyer.jared@uwlax.edu>
2017-05-03 22:17:43 -0500

    Upon deleting an employee, their appointments are deleted as well

--- adminDeleteEmployees.php
+++ adminDeleteEmployees.php
@@ -7,9 +7,11 @@
 
     $employee = $_POST['employee'];
 
-    $sql = "DELETE FROM `Users` WHERE Name = '$employee'";
+    $sql1 = "DELETE FROM `Users` WHERE Name = '$employee'";
+    $sql2 = "DELETE FROM `Calendar` WHERE Employee = '$employee'";
 
-    mysqli_query($mysqli, $sql);
+    mysqli_query($mysqli, $sql1);
+    mysqli_query($mysqli, $sql2);
 
     mysqli_close($mysqli);
 ?>
